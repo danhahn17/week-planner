a33cbc89e348a0fe4fc2f3363b341255784bd121
Christopher Tran <tran.christopher09@gmail.com>
2023-11-17 22:17:56 +0000

    added eventListener for add event and cancel button

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const btnMainModalToggler = document.querySelector('.modalShower')
+const $modalButton = document.querySelector('.modalShower');
+const $modalContainer = document.querySelector('.modal_full');
 
+$modalButton.addEventListener('click', function (event) {
+  $modalContainer.classList.remove('hidden')
+});
 
+const $cancelButton = document.querySelector('.cancel');
 
-
-btnMainModalToggler.addEventListener('click' , () => {
-  btnMainModalToggler.classList.remove('hidden')
-  btnMainModalToggler.classList.add('block')
-})
+$cancelButton.addEventListener('click', function (event) {
+  $modalContainer.classList.add('hidden')
+});
